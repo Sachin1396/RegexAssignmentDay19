6d431a4e7feae2dc62c8e75714c8e4e1999ecd2c
Sachin1396 <sachhushetty1396@gmail.com>
2022-07-20 20:16:51 +0530

    Added:fist password rule

--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -8,28 +8,34 @@ public class UserRegistrationMain {
         System.out.println("2.To Validate the Last Name.");
         System.out.println("3.TO Validate Email id");
         System.out.println("4.To Validate Mobile Number");
+        System.out.println("5. enter the password");
         System.out.println("Enter the option number to perform the operation");
         int option = scanner.nextInt();
         switch (option){
             case 1:
                 System.out.println("enter the first name");
-                String firsName = scanner.nextLine();
+                String firsName = scanner.next();
                 userInformation.validateFirstname(firsName,"^[A-Z]{1}[A-Za-z]{2}$");
                 break;
             case 2:
                 System.out.println("Enter the user last name");
-                String lastName = scanner.nextLine();
+                String lastName = scanner.next();
                 userInformation.validateFirstname(lastName,"^[A-Z]{1}[A-Za-z]{2}$");
                 break;
             case 3:
                 System.out.println("enter the user mail id");
-                String email = scanner.nextLine();
+                String email = scanner.next();
                 userInformation.validateFirstname(email,"[a-zA-z0-9]{2,}[_]@(?:([0-9]{1}|[a-zA-Z]{3,5})\\.)+[a-zA-Z]{2,3}");
                 break;
             case 4:
                 System.out.println("enter the valid phone number0");
-                String number = scanner.nextLine();
+                String number = scanner.next();
                 userInformation.validateFirstname(number,"^[0-9]{2}\\s?[6-9]{1}[0-9]{9}");
+                break;
+            case 5:
+                System.out.println("enter the password");
+                String password = scanner.next();
+                userInformation.validateFirstname(password,"^[a-zA-Z0-9]{8,}$");
                 break;
             default:
                 System.out.println("enter valid option");
